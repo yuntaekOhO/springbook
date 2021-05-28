137039fb2b8975a4d8bf9ea5de861dd5d23e94b3
yuntaekOhO <oyt1996@naver.com>
2021-05-28 17:59:15 +0900

    spring aop annotation (2.5)

--- src/main/java/com/springbook/biz/common/LogAdvice.java
+++ src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,8 +1,14 @@
 package com.springbook.biz.common;
 
 import org.aspectj.lang.JoinPoint;
-
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Service;
+@Service
+@Aspect // Aspect = Pointcut + Advice
 public class LogAdvice {
+	
+	@Before("PointcutCommon.allPointcut()")
 	public void printLog(JoinPoint jp) {
 		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
 	}
